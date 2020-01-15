e11c74c070d2188e269541602fe9575c8f09a6b5
yuchengtao1731 <56573044+yuchengtao1731@users.noreply.github.com>
2020-01-15 19:19:39 +0800

    Create footer.vue

---
+++ chushi/chushi/src/pages/footer/footer.vue
@@ -0,0 +1,32 @@
+<template>
+    <footer>
+        <router-link to="/index">首页</router-link>
+        <router-link to="/classify">分类</router-link>
+        <router-link to="/publish">发布</router-link>
+        <router-link to="/manifest">货单</router-link>
+        <router-link to="/my">我的</router-link>
+    </footer>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="stylus" scoped>
+   footer
+        width 100%
+        display flex
+        background red
+        height .49rem
+        position fixed
+        bottom 0
+        left 0
+        line-height 0.49rem
+        a 
+            flex 1
+            text-align center
+
+</style>
