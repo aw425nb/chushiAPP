92d91d97509c3d2b57faf6d48c237485e5f8af23
wushijie <799754667@qq.com>
2020-01-15 23:11:56 +0800

    awu
    1.添加了详情页
    2.添加了我的供应
    3.添加了我的采购
    4.添加了我的报价
    5.全局引入了Vant  UI组件

--- chushi/chushi/src/main.js
+++ chushi/chushi/src/main.js
@@ -4,6 +4,10 @@ import App from './App.vue'
 import "./assets/rem"
 import "./assets/css/main.styl"
 import createRouter from "./router"
+//引入vant
+import Vant from 'vant';
+import 'vant/lib/index.css';
+Vue.use(Vant);
 
 
 Vue.use(Router)
--- chushi/chushi/src/router/routes.js
+++ chushi/chushi/src/router/routes.js
@@ -1,29 +1,55 @@
-import Index  from "../pages/views/index.vue"
-import My  from "../pages/views/my.vue"
-import Classify  from "../pages/views/classify.vue"
-import Manifest  from "../pages/views/manifest.vue"
-import Publish  from "../pages/views/publish.vue"
+import Index from "../pages/views/index.vue"
+import My from "../pages/views/my.vue"
+import Classify from "../pages/views/classify.vue"
+import Manifest from "../pages/views/manifest.vue"
+import Publish from "../pages/views/publish.vue"
+import MySupply from "../pages/mySupply"
+import Purchase from "../pages/myPurchase"
+import Offer from "../pages/myOffer"
+import Detail from "../pages/detailPage"
 
 
 
 const routes = [{
-    path:"/index",
-    component:Index
-},{
-    path:"/my",
-    component:My
-},{
-    path:"/classify",
-    component:Classify
-},{
-    path:"/manifest",
-    component:Manifest
-},{
-    path:"/publish",
-    component:Publish
-},{
-    path:"*",
-    redirect:"/index"
-}]
+        path: "/index",
+        component: Index
+    },
+    {
+        path: "/my",
+        component: My
+    },
+    {
+        path: "/classify",
+        component: Classify
+    },
+    {
+        path: "/manifest",
+        component: Manifest
+    },
+    {
+        path: "/publish",
+        component: Publish
+    },
+    {//我的供应
+        path: "/mysupply",
+        component: MySupply
+    },
+    {//我的采购
+        path: "/mypurchase",
+        component: Purchase
+    },
+    {//我的报价
+        path: "/myoffer",
+        component: Offer
+    },
+    {//详情页
+        path: "/detail",
+        component: Detail
+    },
+    {
+        path: "*",
+        redirect: "/index"
+    }
+]
 
 export default routes
