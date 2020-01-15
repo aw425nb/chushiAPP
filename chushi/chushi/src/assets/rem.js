0cf3a6042a1fa4cafdf503d4b7d19ce1967034bb
yuchengtao1731 <56573044+yuchengtao1731@users.noreply.github.com>
2020-01-15 19:19:51 +0800

    Create rem.js

---
+++ chushi/chushi/src/assets/rem.js
@@ -0,0 +1,8 @@
+function rem(){
+    const doce= document.documentElement;
+    doce.style.fontSize= (doce.clientWidth/7.5)*2+"px"
+}
+
+
+addEventListener("load",rem)
+addEventListener("resize",rem)
