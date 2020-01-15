47e2ba1911d514ec504e2c3945cc0a94d116ce93
Nobel-Guang <54129513+Nobel-Guang@users.noreply.github.com>
2020-01-15 15:21:59 +0800

    Create wenjian.vue
    广业东是个dog

---
+++ chushi/chushi/src/pages/wenjian.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+guangyedongshigegou
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+ }
+</script>
+
+<style scoped>
+
+
+</style>
