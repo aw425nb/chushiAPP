c2c8b3a03fa3a9c95d9392e7fc7d6253c3416ac7
hanyue <727707015@qq.com>
2020-01-16 18:57:22 +0800

    Create PostPurchase.vue
    添加分类导航页面，改动footer、header、search组件路径位置--韩悦

---
+++ chushi/chushi/src/pages/publish/PostPurchase.vue
@@ -0,0 +1,21 @@
+<template>
+ <div>
+<!-- 点击发布采购页面 PostPurchase-->
+发布采购页面
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+ }
+</script>
+
+<style scoped>
+
+
+</style>
