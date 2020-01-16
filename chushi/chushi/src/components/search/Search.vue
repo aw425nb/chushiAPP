66ace5ef4fd56c15ec7bd7dac457ed17c84c3f01
hanyue <727707015@qq.com>
2020-01-16 18:57:03 +0800

    Create Search.vue
    添加分类导航页面，改动footer、header、search组件路径位置--韩悦

---
+++ chushi/chushi/src/components/search/Search.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="search">
+        <div class="search-top">
+            <!-- 后退箭头 -->
+            <van-icon name="arrow-left" />
+            <SearchBox/>
+        </div>
+        <div class="menu"><DropMenu/></div>
+    </div>
+</template>
+
+<script>
+//引入搜索框
+import SearchBox from "./SearchBox.vue"
+import DropMenu from "./DropMenu.vue"
+export default {
+    data(){
+        return {
+            
+        }
+    },
+    components:{
+        SearchBox,
+        DropMenu,
+    }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+.search
+    display flex
+    flex-direction column
+    height 100%
+    width 100%
+    .search-top
+        width 100%
+        padding .05rem .1rem
+        display flex
+        justify-content center
+        align-items center
+.menu 
+    width 100%
+    height .5rem
+
+</style>
