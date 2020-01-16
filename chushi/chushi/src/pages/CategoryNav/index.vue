7a2eb88e63b7f8835054649580783bcc6f9dd730
hanyue <727707015@qq.com>
2020-01-16 18:57:11 +0800

    Create index.vue
    添加分类导航页面，改动footer、header、search组件路径位置--韩悦

---
+++ chushi/chushi/src/pages/CategoryNav/index.vue
@@ -0,0 +1,20 @@
+<template>
+ <div>
+<!-- 分类导航 CategoryNav -->
+ </div>
+</template>
+
+<script>
+ export default {
+   data () {
+     return {
+
+     }
+   },
+ }
+</script>
+
+<style scoped>
+
+
+</style>
