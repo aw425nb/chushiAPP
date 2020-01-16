0ff4cade6b13b6dab0be21ffbff2432029952a5d
hanyue <727707015@qq.com>
2020-01-16 20:51:36 +0800

    韩悦导航分类

--- chushi/chushi/src/pages/CategoryNav/content.vue
+++ chushi/chushi/src/pages/CategoryNav/content.vue
@@ -1,21 +1,47 @@
 <template>
- <div>
-    导航分类 content
-<!-- 导航分类 content -->
- </div>
+  <div>
+    <!-- 导航分类 content -->
+    <!-- 一级导航 -->
+    <van-tree-select
+      heigh="100%"
+      :items="items"
+      :active-id.sync="activeId"
+      :main-active-index.sync="activeIndex"
+    >
+      <template slot="content">
+        <Floor></Floor>
+      </template>
+    </van-tree-select>
+  </div>
 </template>
 
 <script>
- export default {
-   data () {
-     return {
-
-     }
-   },
- }
+import Floor from "./floor";
+export default {
+  components: {
+    Floor
+  },
+  data() {
+    return {
+      activeId: 1,
+      activeIndex: 0,
+      items: [
+        { text: "水果" },
+        { text: "蔬菜" },
+        { text: "食用菌" },
+        { text: "水产" },
+        { text: "畜禽" },
+        { text: "干果" },
+        { text: "中药材" },
+        { text: "茶叶" },
+        { text: "其它" }
+      ]
+    };
+  }
+};
 </script>
 
-<style scoped>
-
-
+<style scoped lang="stylus">
+.van-tree-select
+    margin-top .13rem
 </style>
---
+++ chushi/chushi/src/pages/CategoryNav/floor.vue
@@ -0,0 +1,98 @@
+<template>
+  <!-- 二级导航
+  item.title: 当季最热、其他
+  fid: 一级导航的id
+  sid: 二级导航的id
+  sname: 二级导航分类的标题
+  url: 二级导航分类的图片路径-->
+  <div class="floor">
+    <div class="hot" v-for="item of hotlist" :key="item.fid">
+      <h3>{{item.title}}</h3>
+      <ul>
+        <li v-for="ele of item.list" :key="ele.sid">
+          <a href>
+            <img :src="ele.url" alt />
+            <span>{{ele.sname}}</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      hotlist: [
+        {
+          title: "当季最热",
+          fid: 1,
+          list: [
+            {
+              sid: 1,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "葡萄"
+            },
+            {
+              sid: 2,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "西瓜"
+            },
+            {
+              sid: 3,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "蜜桃"
+            },
+            {
+              sid: 4,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "苹果"
+            },
+            {
+              sid: 5,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "梨"
+            },
+            {
+              sid: 6,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "李子"
+            },
+            {
+              sid: 7,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "猕猴桃"
+            },
+            {
+              sid: 8,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "柑橘"
+            },
+            {
+              sid: 9,
+              url:
+                "https://img.alicdn.com/imgextra/i4/2053469401/O1CN01MeXEEF2JJhzeoqBBw_!!2053469401.png",
+              sname: "鲜枣"
+            }
+          ]
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped lang="stylus">
+.floor
+    li
+        width .3rem
+</style>
