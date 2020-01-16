3a19a36440bb4c9110f6eac4d385843fd000d6d1
yuchengtao1731 <56573044+yuchengtao1731@users.noreply.github.com>
2020-01-16 09:39:30 +0800

    Update App.vue

--- chushi/chushi/src/App.vue
+++ chushi/chushi/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <router-view/>
-    <Footer v-if="isshow"></Footer>
+    <Footer v-if="$route.meta.footerShow"></Footer>
   </div>
 </template>
 <script>
@@ -10,14 +10,6 @@ import Footer from "./pages/footer/footer.vue"
 export default {
   components:{
     Footer
-  },
-  data () {
-    return {
-      isshow:true
-    }
-  },
-  mounted () {
-    this.isshow = this.$route.meta.isshow
   }
   
 }
