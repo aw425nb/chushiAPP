2b52826252262aaafb864c741639515e7a7d430f
yuchengtao1731 <56573044+yuchengtao1731@users.noreply.github.com>
2020-01-16 20:38:41 +0800

    Update index.vue

--- chushi/chushi/src/pages/views/index.vue
+++ chushi/chushi/src/pages/views/index.vue
@@ -1,5 +1,22 @@
+
 <template>
-    <div>我是首页
+    <div class="index" style="background:#f6f6f6">
+       
+        <div class="search">
+            <div class="logo">
+                <img src="../../assets/img/logo.jpg" alt="">
+                
+            </div>
+            <div class="searchmain">
+                <van-icon name="search" />
+                搜好货
+            </div>
+            <div class="city" v-cloak>
+                <van-icon name="location" />
+                {{LocationCity}}
+            </div>
+          
+        </div>
      
 
 
@@ -10,14 +27,60 @@
 
 
 
-<script>
-
-export default {
-    
-}
-
-</script>
+ <script> 
+ export default{
+    data(){
+        return{
+            LocationCity:"....."     //给渲染层定义一个初始值
+        }
+    },
+    created(){
+        
+        this.$city() //触发获取城市方法
+    }
+ }
+ </script>
 
 <style lang="stylus" scoped>
-
+    .search
+        height .5rem
+        width 100%
+        padding .09rem .16rem
+        display flex
+        align-items center
+        justify-content space-between
+        .logo
+            width .38rem
+            height .32rem
+            img 
+                display  block
+                width .38rem
+                height .18rem
+                margin-top .08rem
+        .searchmain
+            width 2.22rem
+            height .32rem
+            border-radius .1rem
+            padding 0 .3rem
+            line-height  .32rem
+            color #999999
+            position relative
+            background #fff
+            .van-icon
+                position absolute
+                font-size .2rem 
+                top 0.065rem
+                left 0.28em
+        .city
+            width .65rem
+            height .32rem
+            position relative
+            padding 0 0 0 0.17rem
+            line-height .32rem
+            cursor pointer
+            .van-icon
+                position absolute
+                font-size .2rem 
+                top 0.065rem
+                left -0.16em
 </style>
