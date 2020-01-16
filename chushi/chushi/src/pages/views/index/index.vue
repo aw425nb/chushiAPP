e86401c3ac44ad9481244a497970b7c3fe26cf07
yuchengtao1731 <56573044+yuchengtao1731@users.noreply.github.com>
2020-01-16 22:30:11 +0800

    Create index.vue

---
+++ chushi/chushi/src/pages/views/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Indexsearch/>
+        <Banner/>
+    </div>
+</template>
+
+
+<script>
+import Indexsearch from "./Indexsearch.vue"
+import Banner from "./banner.vue"
+export default {
+    components:{
+        Indexsearch,
+        Banner
+    }
+}
+</script>
+
+
+
+<style lang="stylus" scoped>
+
+</style>
